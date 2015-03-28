4cd61b50676a377c5379d76ec0ac9b7ce8881672
ibnHatab <lib.aca55a@gmail.com>
2015-03-28 14:50:30 +0000

    refactor static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # Return full title on per page basis.
+  def full_title(page_title = '')
+    base_tittle = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_tittle
+    else
+      "#{page_title} | #{base_tittle}"
+    end
+  end
 end
