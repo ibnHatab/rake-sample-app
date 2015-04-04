f9800a238ca6091df2f0338252521fb8f1b1f51a
VKI <lib.aca55a@gmail.com>
2015-04-04 18:08:50 +0300

    fix SNMP auth

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
                                         address: 'smtp.gmail.com',
                                         port: '587',
-                                        authentication: 'plaine',
+                                        authentication: 'plain',
                                         enable_starttls_auto: true,
+                                        domain:       'heroku.com',
                                         user_name: ENV['GMAIL_USERNAME_DEV'],
                                         password: ENV['GMAIL_PASSWORD_DEV']
                                        }
