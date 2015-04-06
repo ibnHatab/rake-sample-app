aaae24f12b79d90ad53e15cc17c76f9c3141c1cb
VKI <lib.aca55a@gmail.com>
2015-04-06 12:36:50 +0300

    cleaning

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,6 @@ Rails.application.configure do
                                         password: ENV['GMAIL_PASSWORD_DEV']
                                        }
 
-
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
